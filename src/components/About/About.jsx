735d0ce281bdd10de1d270de5c6ade876f416000
konain1 <kaunainkasab@gmail.com>
2023-08-30 12:33:12 +0530

    about page all done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "./../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
 function About() {
   return (
-   <section id='about'>about</section>
-  )
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="me" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>2+ years working </small>
+            </article>
+
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>2+ years working </small>
+            </article>
+
+            <article className="about__card">
+              {/* <FaAward className='about__icon'/> */}
+              <AiOutlineFundProjectionScreen className="about__icon" />
+              <h5>Projects</h5>
+              <small>Find Them All Here</small>
+            </article>
+          </div>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book.
+          </p>
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
